7952852bb1d10de96531394ea86bdae336e4308b
Chocoment <tintinch59@gmail.com>
2021-10-20 17:25:49 +0100

    - Added Spawners

---
+++ CetteFoisCLaBonne/Assets/Scripts/CocoScripts/Spawner.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject obstacle;
+
+    private float timeBtwSpawn;
+    public float startTimeBtwSpawn;
+    public float decreaseTime;
+    public float minTime;
+
+    private void Update()
+    {
+        if(timeBtwSpawn <= 0)
+        {
+            Instantiate(obstacle, transform.position, Quaternion.identity);
+            timeBtwSpawn = startTimeBtwSpawn;
+            if(startTimeBtwSpawn > minTime)
+            {
+                startTimeBtwSpawn -= decreaseTime;
+            }
+        } else
+        {
+            timeBtwSpawn -= Time.deltaTime;
+        }
+    }
+}
