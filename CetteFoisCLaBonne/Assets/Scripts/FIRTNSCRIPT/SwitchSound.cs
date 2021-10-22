dce0a0ef55e875499d0f0d8e38b29eb9e251e563
Chocoment <tintinch59@gmail.com>
2021-10-22 13:57:12 +0100

    - Integrated SoundDesignV2

--- CetteFoisCLaBonne/Assets/Scripts/FIRTNSCRIPT/SwitchSound.cs
+++ CetteFoisCLaBonne/Assets/Scripts/FIRTNSCRIPT/SwitchSound.cs
@@ -4,21 +4,11 @@ using UnityEngine;
 
 public class SwitchSound : MonoBehaviour
 {
-<<<<<<< HEAD
-    public GameObject sound;
-    public GameObject backSound;
-=======
->>>>>>> Hector
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-<<<<<<< HEAD
-            sound.SetActive(true);
-            backSound.SetActive(false);
-=======
             FindObjectOfType<AudioManager>().Switch();
->>>>>>> Hector
             Destroy(gameObject);
         }
     }
